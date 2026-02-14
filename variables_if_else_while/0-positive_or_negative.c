f8fe49927468849d1ae8b3784bf0963a8666ccc6
root <root@ip-10-42-233-253.ec2.internal>
2026-02-14 21:35:11 +0000

    added spaces

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- *main - prints whether a number is positive or negative
+ * main - prints whether a number is positive or negative
  *
- *Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
