07b858c67a55e659028e1d9ddd9844a4574674ae
root <root@ip-10-42-233-253.ec2.internal>
2026-02-15 01:49:55 +0000

    dded program that spells alphabet in reverse

---
+++ variables_if_else_while/7-print_tabahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints tebahapla
+ *
+ * Return: Always 0 (Success)
+ */
+{
+	int letra;
+
+	for (letra = 'z'; letra >= 'a'; letra--)
+	{
+		putchar(letra);
+	}
+	putchar('\n');
+
+	return (0);
+}
