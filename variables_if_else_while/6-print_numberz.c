b40d81a5551092a137b0908257c94e48fad39d13
root <root@ip-10-42-233-253.ec2.internal>
2026-02-15 01:35:08 +0000

    added program that prints numbers withount printf command

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 10 without printf command
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
