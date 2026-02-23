36e39c4e5612a95154bf6c1e36adc373efa81125
root <root@ip-10-42-115-83.ec2.internal>
2026-02-23 00:21:28 +0000

    Add print_array function

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to the first element of the array
+ * @n: number of elements to print
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		/* If not the last element, print comma and space */
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
